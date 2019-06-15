71c2594a45b79a67720eb200895fad909aa2e9a2
kenken <kentaroy@stanford.edu>
2019-06-15 23:16:42 +0900

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -6,10 +6,14 @@ import seaborn as sns
 
 df = pd.read_csv('fish.csv', index_col=0)
 
+# 元データを視る
+# print(df)
+
+# 解析したい魚種を入力
+# ex. "アジ", "シコイワシ"など
 dfana = df[df.fishname == "シコイワシ"]
 
+# 相関をプロット
 pg = sns.pairplot(dfana)
 print(type(pg))
 
-#plt.matshow(df.corr())
-#plt.show()
